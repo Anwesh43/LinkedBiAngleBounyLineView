613300b54f23b61d2e62f0dc802cae3decafc333
Anwesh43 <anweshthecool0@gmail.com>
2019-12-28 14:07:32 +0530

    adding BiAngleBouncyLineView class

--- bianglebouncylineview/src/main/java/com/anwesh/uiprojects/bianglebouncylineview/BiAngleBouncyLineView.kt
+++ bianglebouncylineview/src/main/java/com/anwesh/uiprojects/bianglebouncylineview/BiAngleBouncyLineView.kt
@@ -55,3 +55,21 @@ fun Canvas.drawBABLNode(i : Int, scale : Float, paint : Paint) {
     drawBiAngleBouncyLine(scale, size, paint)
     restore()
 }
+
+class BiAngleBouncyLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
